f133d43df241a238ca36852c69fdaf4992943687
Snell <ljsnell@116366NW.local>
2013-10-28 21:04:02 -0500

    Added Button for Sign in

---
+++ TreeHouse/TreeHouse/WelcomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  WelcomeViewController.m
+//  TreeHouse
+//
+//  Created by Snell, Luke J on 2013-10-28.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import "WelcomeViewController.h"
+
+@interface WelcomeViewController ()
+
+@end
+
+@implementation WelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
