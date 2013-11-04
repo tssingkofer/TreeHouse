fbb7e45a6b4d361b58884e2c0960ba64c07222ee
Christopher Dadisman <dadisman@DadismanMBP.local>
2013-11-04 17:30:28 -0600

    Added my stuff

---
+++ TreeHouse/TreeHouse/CheckInViewController.h
@@ -0,0 +1,14 @@
+//
+//  CheckInViewController.h
+//  TreeHouse
+//
+//  Created by Christopher Dadisman on 11/4/13.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckInViewController : UIViewController
+
+@property (copy, nonatomic) NSArray *youth;
+@end
---
+++ TreeHouse/TreeHouse/CheckInViewController.m
@@ -0,0 +1,57 @@
+//
+//  CheckInViewController.m
+//  TreeHouse
+//
+//  Created by Christopher Dadisman on 11/4/13.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import "CheckInViewController.h"
+
+@interface CheckInViewController ()
+
+@end
+
+@implementation CheckInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"Check-in";
+        //self.youth = "";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    
+}
+-(void) viewDidAppear:(BOOL)animated
+{
+    
+}
+-(void) viewWillDisappear:(BOOL)animated
+{
+    
+}
+-(void)viewDidDisappear:(BOOL)animated
+{
+    
+}
+@end
