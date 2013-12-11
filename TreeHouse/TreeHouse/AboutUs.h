b4f7faa8899bcb9fde65792c5dbdd87c87e62861
Christopher Dadisman <dadisman@DadismanMBP.local>
2013-12-11 15:19:32 -0600

    Changed the about-us view controller to its own nib

---
+++ TreeHouse/TreeHouse/AboutUs.h
@@ -0,0 +1,13 @@
+//
+//  AboutUs.h
+//  TreeHouse
+//
+//  Created by Christopher Dadisman on 12/11/13.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUs : UIViewController
+
+@end
---
+++ TreeHouse/TreeHouse/AboutUs.m
@@ -0,0 +1,38 @@
+//
+//  AboutUs.m
+//  TreeHouse
+//
+//  Created by Christopher Dadisman on 12/11/13.
+//  Copyright (c) 2013 Tyler Singkofer. All rights reserved.
+//
+
+#import "AboutUs.h"
+
+@interface AboutUs ()
+
+@end
+
+@implementation AboutUs
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
